4f9f27b06d349fb545e7c82dc8c75cb0b1457a01
Supahmonke <alex.etelaniemi@gmail.com>
2023-06-11 00:26:33 +0300

    main page css

--- LarpNet/MainPage/main.php
+++ LarpNet/MainPage/main.php
@@ -4,28 +4,28 @@
     if (isset($_SESSION['Username'])) {
     
     } else {
-        header('Location: ../LoginPage/login.php');
+        //header('Location: ../LoginPage/login.php');
     }
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" /> 
-        <link rel="stylesheet" href="main.css">
-    </head>
-    <body>
-        <div id="center">
-            <div id="top-bar">
-                <nav>
-                    <ul id="Buttons">
-                    <input type="button" id="button" onclick="window.location.href='../SubPages/PersonalInfo/Personal.php';" value="Omat tiedot"></button>
-                    <input type="button" id="button" onclick="window.location.href='../SubPages/Banking/Bank.php';" value="Inter Galaktinen Pankki"></button>
-                    <input type="button" id="button" onclick="window.location.href='../SubPages/Encyclopedia/Encyclopedia.php';" value="Tietokirja"></button>
-                    <input type="button" id="button" onclick="window.location.href='../SubPages/StarMaps/Maps.php';" value="Tähtikartat"></button>
-                    </ul>
-                </nav>    
-            </div>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+  <link rel="stylesheet" href="main.css">
+  <title>Main Page</title>
+</head>
+<body>
+  <div id="center">
+  <nav class="btn-container">
+    <button class="btn"><span class="btn__content"><a href="../SubPages/PersonalInfo/Personal.php">Personal info</a></span></button>
+    <button class="btn"><span class="btn__content"><a href="../SubPages/Banking/Bank.php">Inter Galactic Bank</a></span></button>
+    <button class="btn"><span class="btn__content"><a href="../SubPages/Notes/Notes.php">Notepad</a></span></button>
+    <button class="btn"><span class="btn__content"><a href="../SubPages/Messenger/Messenger.php">Messenger</a></span></button>
+    <button class="btn"><span class="btn__content"><a href="../SubPages/CrewLog/CrewLog.php">Crew log</a></span></button>
+</nav>
+
+>>>>>>> Stashed changes
 
             <div id="NewsContainer">
                 <h1>Intergalaktiset uutiset:<h1>
