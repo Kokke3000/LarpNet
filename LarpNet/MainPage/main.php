8b9036122c362380c6267df9d4b9a87edcb66406
Kokke3000 <kokke99@gmail.com>
2023-06-11 21:30:59 +0300

    Minor fixes

--- LarpNet/MainPage/main.php
+++ LarpNet/MainPage/main.php
@@ -29,25 +29,6 @@
     <button class="btn"><span class="btn__content"><a href="../SubPages/CrewLog/CrewLog.php">Crew log</a></span></button>
 </nav>
 
->>>>>>> Stashed changes
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1" /> 
-  <link rel="stylesheet" href="main.css">
-  <title>Main Page</title>
-</head>
-<body>
-  <div id="center">
-    <div id="top-bar">
-      <nav>
-            <button class="btn"><span class="btn__content"><a href="../SubPages/PersonalInfo/Personal.php">Personal info</a></span></button>
-            <button class="btn"><span class="btn__content"><a href="../SubPages/Banking/Bank.php">Inter Galactic Bank</a></span></button>
-            <button class="btn"><span class="btn__content"><a href="../SubPages/Notes/Notes.php">Notepad</a></span></button>
-            <button class="btn"><span class="btn__content"><a href="../SubPages/Messenger/Messenger.php">Messenger</a></span></button>
-            <button class="btn"><span class="btn__content"><a href="../SubPages/CrewLog/CrewLog.php">Crew log</a></span></button>
-        
-      </nav>    
-    </div>
-
   <div id="Admin">
   <button class="btn" <?php echo $style;?>><span class="btn__content"><a href="../SubPages/Admin/Admin.php">Admin controlls</a></span></button>
   </div>
@@ -55,7 +36,7 @@
     <div id="NewsContainer">
       <h1>Intergalactic news:</h1>
       <div id="News">
-        <p>Tämä on testiartikkeli</p>
+        <p>Error 404: Network timed out, check your interstellarnet connection</p>
       </div>
     </div>
   </div>
